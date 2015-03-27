4f2cfdbc876fcd0e554265198b4328fc6cea5694
Cleggameg <cleggameg@gmail.com>
2015-03-27 18:48:01 -0500

    Who let the dogs out? BRCRPT

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ActiveRecord::Base
   has_many :surveys, foreign_key: 'creator_id'
   has_many :taken_surveys, foreign_key: "participant_id"
   has_many :responses, through: :taken_surveys
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(pass)
+    @password ||= Password.create(pass)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, pass)
+    @user = User.find_by(email: email)
+    return @user if @user && (@user.password == pass)
+  end
+
 end
