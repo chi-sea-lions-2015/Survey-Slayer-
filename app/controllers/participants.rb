6e37a1304d314ca6bd5a69c95bd9f072dc7c5503
Chris Lintecum <clintecum2@gmail.com>
2015-03-29 14:58:29 -0500

    fixed route

--- app/controllers/participants.rb
+++ app/controllers/participants.rb
@@ -8,6 +8,7 @@ end
 get '/participants/survey/:id' do
   @survey = Survey.find_by(id: params[:id])
   @survey_id = @survey.id
+  @questions = @survey.questions
   erb :"participants/surveys"
 end
 
@@ -15,7 +16,7 @@ post '/participants/survey/:id/response' do
   survey = Survey.find_by(id: params[:id])
   puts params
   user = current_user
-  @taken_survey = survey.taken_surveys.create(participant: user)
-  @response = @taken_survey.responses.create(params[:choice])
+  taken_survey = survey.taken_surveys.create(participant: user)
+  response = taken_survey.responses.create(params[:choice])
   redirect "/surveys"
 end
