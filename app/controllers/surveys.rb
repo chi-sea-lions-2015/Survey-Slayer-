aa8b79c8cd2cd95ba61c25167c92418882c1103b
Cleggameg <cleggameg@gmail.com>
2015-03-28 21:43:49 -0500

    got first ajax working

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -1,6 +1,6 @@
 get '/surveys' do
   @surveys = Survey.all
- erb :surveys
+  erb :surveys
 end
 
 get '/my_surveys' do
@@ -19,34 +19,60 @@ end
 
 post '/surveys' do
   @user = current_user
+  puts params
   @survey = @user.surveys.create(params[:survey])
-  redirect "/surveys/#{@survey.id}/questions/new"
-end
+  title = @survey.title
+  puts title
+  survey_id = @survey.id
+  if request.xhr?
+    content_type :json
+    return {question_form: erb(:'questions/new', layout: false),
+      title: @survey.title,
+      survey_id: survey_id}.to_json
+    else
+      redirect "/surveys/#{@survey.id}/questions/new"
+    end
+  end
 
-get '/surveys/:survey_id/questions/new' do
-  @survey = Survey.find_by(id: params[:survey_id])
-  erb :"questions/new"
-end
+  get '/surveys/:survey_id/questions/new' do
+    @survey = Survey.find_by(id: params[:survey_id])
+    erb :"questions/new"
+  end
 
-post '/surveys/:survey_id/questions' do
-  @survey = Survey.find_by(id: params[:survey_id])
-  @question = @survey.questions.create(params[:question])
-  redirect "/surveys/#{@survey.id}/questions/#{@question.id}/choices/new"
-end
+  post '/surveys/:survey_id/questions' do
+    @survey = Survey.find_by(id: params[:survey_id])
+    @question = @survey.questions.create(params[:question])
+    @choice1 = @question.choices.create(params[:choice1])
+    @choice2 = @question.choices.create(params[:choice2])
+    @choice3 = @question.choices.create(params[:choice3])
+    @choice4 = @question.choices.create(params[:choice4])
+    if request.xhr?
+      content_type :json
+      return {question_form: erb(:'questions/new', layout: false),
+        question: @question.title,
+        choice1: @choice1.title,
+        choice2: @choice2.title,
+        choice3: @choice3.title,
+        choice4: @choice4.title}.to_json
+    else
+      redirect "/surveys/#{@survey.id}/questions/new"
+    end
+  end
 
-get '/surveys/:survey_id/questions/:question_id/choices/new' do
-  @survey= Survey.find_by(id: params[:survey_id])
-  @question = Question.find_by(id: params[:question_id])
-  erb :'choices/new'
-end
 
-post '/surveys/:survey_id/questions/:question_id/choices' do
-  @survey = Survey.find_by(id: params[:survey_id])
-  @question = Question.find_by(id: params[:question_id])
-  @choice1 = @question.choices.create(params[:choice1])
-  @choice2 = @question.choices.create(params[:choice2])
-  @choice3 = @question.choices.create(params[:choice3])
-  @choice4 = @question.choices.create(params[:choice4])
-  redirect "/surveys"
-end
+    get '/surveys/:survey_id/questions/:question_id/choices/new' do
+      @survey= Survey.find_by(id: params[:survey_id])
+      @question = Question.find_by(id: params[:question_id])
+      erb :'choices/new'
+    end
+
+    post '/surveys/:survey_id/questions/:question_id/choices' do
+      @survey = Survey.find_by(id: params[:survey_id])
+      @question = Question.find_by(id: params[:question_id])
+      @choice1 = @question.choices.create(params[:choice1])
+      @choice2 = @question.choices.create(params[:choice2])
+      @choice3 = @question.choices.create(params[:choice3])
+      @choice4 = @question.choices.create(params[:choice4])
+      redirect "/surveys"
+    end
 
--- public/js/application.js
+++ public/js/application.js
@@ -3,20 +3,44 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
-  // $("#survey_create_form").submit(function(e){
-  //   e.preventDefault();
-  //   var form = $(this);
-  //   console.log(this);
-  //   $.ajax({
-  //     "url": "/surveys",
-  //     "method": "post",
-  //     "data": form.serialize(),
-  //     "dataType": "json",
-  //     'success': function(response){
-  //       $("").append(response);
-  //     }
-  //   });
-  // });
+  $("#survey_create_form").submit(function(e){
+    e.preventDefault();
+    var form = $(this);
+    console.log(this);
+    $.ajax({
+      "url": "/surveys",
+      "method": "post",
+      "data": form.serialize(),
+      'success': function(response){
+        console.log(response);
+        $("#survey_create_form").css({display: "none"});
+        $("body").append(response.question_form)
+        $("#heading").text(response.title)
+        surveyId = response.survey_id
+      },
+      'complete': function(response){
+        console.log(response)
+        // $("body").append(response)
+      },
+
+    });
+  });
+
+  $("#question_create_form").submit(function(e){
+    e.preventDefault();
+    var form = $(this);
+    console.log(this);
+    $.ajax({
+      "url": "/surveys/" + surveyId + "/questions",
+      "method": "post",
+      "data": form.serialize(),
+      'success': function(response){
+        console.log(response);
+
+      }
+
+    });
+  });
 
 
 });
